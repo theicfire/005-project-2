33f80abc944ec2c1d545683484e5eb7d7831c0e0
Thomas W Schultz <tschultz@mit.edu>
2012-05-05 14:48:33 -0400

    sdjopsdokfopsdf

--- src/main/Client.java
+++ src/main/Client.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -20,17 +22,33 @@ public class Client {
 	private static ConcurrentHashMap<String, ArrayBlockingQueue<Message>> chats
 		= new ConcurrentHashMap<String, ArrayBlockingQueue<Message>>();
 	private String username;
+	private static BuddyList buddyList;
+	
+	public Client(){
+		//Make a login screen, which for now will just print it out, but will eventually call .login(String username)
+		// For now....
+		login("tschultz");
+	}
+	
+	public void login(String username){
+		this.username = username;
+		buddyList = new BuddyList(username);
+	}
+	
 	
 	/* Handles connection messages by calling loggedIn/loggedOut.
 	 */
 	public static void handleConnectionMessage(ConnectionMessage message) {
 		if (message.type == ConnectionMessage.types.CONNECT){
-			loggedIn(message.getFromUsername());
+			buddyList.buddyLogin(message.getFromUsername());
+			
 		} else if (message.type == ConnectionMessage.types.DISCONNECT){
-			loggedOut(message.getFromUsername());
+			buddyList.buddyLogout(message.getFromUsername());
 		}
+		updateOnline();
 	}
 	
+
 	/* Handles Request messages by calling acceptRequest.
 	 * If the request is accepted, it loggedIn/loggedOut.
 	 */
@@ -59,21 +77,9 @@ public class Client {
 		}
 	}
 
-	/* Called by handleConnectionMessage, when a user logs in, and a CONNECT message
-	 * is sent to the client from the server. Logs the user in on the GUI. 
-	 */
-	private static void loggedOut(String fromUsername) {
+	private static void updateOnline() {
 		// TODO Auto-generated method stub
 	}
-	
-	/* Called by handleConnectionMessage, when a user logs out, and a DISCONNECT
-	 * is sent to the client from the server. Logs the user out on the GUI. 
-	 */
-	private static void loggedIn(String fromUsername) {
-		// TODO Auto-generated method stub
-		
-	}
-	
 	
 	/* Called by handleRequestMessage, when a user requests to start a chat, and a REQUEST
 	 * is sent to the client from the server. Opens up a popup window seeing if they want to
@@ -89,19 +95,20 @@ public class Client {
 	 * Start a GUI chat client.
 	 */
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// prompt the user to enter their name
-		Socket socket = new Socket("localhost", 4444);
-		// probably won't use this variable; everything happens inside of the thread
-		ReceiveFromServerConnection receiver = new ReceiveFromServerConnection(socket);
-		receiver.start();
-		ArrayBlockingQueue<Message> queue = new ArrayBlockingQueue<Message>(1000);
-		SendToClientConnection sender = new SendToClientConnection(socket, queue);
-		sender.start();
-
-		for (String line = br.readLine(); line != null; line = br.readLine()) {
-			System.out.println("Given line: " + line);
-			queue.offer(new TextMessage("chase", "tim", "hey tim!")); // this sends to the sender because the objects are shared
-		}
+		Client c = new Client();
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		// prompt the user to enter their name
+//		Socket socket = new Socket("localhost", 4444);
+//		// probably won't use this variable; everything happens inside of the thread
+//		ReceiveFromServerConnection receiver = new ReceiveFromServerConnection(socket);
+//		receiver.start();
+//		ArrayBlockingQueue<Message> queue = new ArrayBlockingQueue<Message>(1000);
+//		SendToClientConnection sender = new SendToClientConnection(socket, queue);
+//		sender.start();
+//
+//		for (String line = br.readLine(); line != null; line = br.readLine()) {
+//			System.out.println("Given line: " + line);
+//			queue.offer(new TextMessage("chase", "tim", "hey tim!")); // this sends to the sender because the objects are shared
+//		}
 	}
 }
